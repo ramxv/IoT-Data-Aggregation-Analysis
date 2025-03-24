93ad1071b87130680f35436b0644a0c4a159b03a
Ram Singh <ramxv.singh@gmail.com>
2025-03-24 13:24:01 +0100

    Update commenting, send_sensor_data function, validations and imports

--- sensors/sensordata.py
+++ sensors/sensordata.py
@@ -1,9 +1,13 @@
-import requests
+import urllib.request
+import urllib.parse
+import json
 import random
+import time
 import sys
 from datetime import datetime
 
 # 1. Variables Declaration
+URL = "http://flask_server:8080/store"
 sensor_type_argument = sys.argv[1]
 sensor_type = ["temperature", "pressure", "air-quality", "co2"]
 sensor_unit = {
@@ -12,9 +16,14 @@ sensor_unit = {
     "air-quality": "PM10",
     "co2": "ppm",
 }
+# 2. Validation of argument in case the user forget to introduce a sensor type
+if len(sys.argv) > 1:
+    sensor_type_argument = sys.argv[1]
+else:
+    print("No sensor type provided. Please introduce a sensor type.")
+    sys.exit(1)
 
-
-# 2. Declarate function for each sensor
+# 3. Declarate function for each sensor
 def temperature_sensor(argv):
     sensor_value = round(random.uniform(-30, 20), 2)
     send_sensor_data(argv, sensor_value)
@@ -34,24 +43,37 @@ def co2_sensor(argv):
     sensor_value = random.randint(400, 5000)
     send_sensor_data(argv, sensor_value)
 
+# 4. Send the sensor data periodically (within 5 seconds) 
+def send_sensor_data(sensor_type, sensor_value):
+    while True:
+        sensor_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M")
+        sensor_latitud = random.randint(-90, 90)
+        sensor_longitud = random.randint(-180, 180)
 
-def send_sensor_data(sensor_type, data):
-    sensor_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M")
-    sensor_latitud = random.randint(-90, 90)
-    sensor_longitud = random.randint(-180, 180)
-    data = {
-        "sensor_type": sensor_type,
-        "unit": sensor_unit[sensor_type],
-        "sensor_location_lat": sensor_latitud,
-        "sensor_location_lon": sensor_longitud,
-        "timestamp": sensor_timestamp,
-        "value": data,
-    }
-    response = requests.post("http://127.0.0.1:8080/store", json=data)
-    print(f"Sent data: {data}, Response: {response.status_code}")
+        data = {
+            "sensor_type": sensor_type,
+            "unit": sensor_unit[sensor_type],
+            "sensor_location_lat": sensor_latitud,
+            "sensor_location_lon": sensor_longitud,
+            "timestamp": sensor_timestamp,
+            "value": sensor_value,  
+        }
+				#	4.1 Send the POST request to the flask server 
+        json_data = json.dumps(data).encode("utf-8")         
+        headers = {"Content-Type": "application/json"}  
+
+        try:
+            request = urllib.request.Request(
+                URL, data=json_data, headers=headers, method="POST"
+            )
+            response = urllib.request.urlopen(request)
+            print(f"Sent data: {data}, Response: {response.read().decode()}")
+        except Exception as e:
+            print(f"Error with the data: {e}")
+        time.sleep(5)  # Send data every 5 seconds
 
 
-# 3. Validation of the arguments passed through cli
+# 6. Validation of the arguments passed through cli
 if len(sensor_type_argument) > 1:
     if sensor_type_argument in sensor_type:
         if sensor_type_argument == "temperature":
